f3304342d43b63aff25f70bf562ac6f27808544f
David Goelzhaeuser <me@idoodler.de>
2015-02-27 18:45:34 +0100

    Update POEditor.h
    - Added a few new functions

--- POEditor.h
+++ POEditor.h
@@ -21,12 +21,65 @@
 /**
  Returns the translated string for "key" if there is no string for "key", "key" will be returned instead.
  @param key This is string you like to get tle localized string for.
+ @return NSString
 */
 +(NSString *)localizedStringWithKey:(NSString *)key;
 
 /**
  Return the Dictionary of Contrubutors in this formate @{@[Name, Email], Language}, e.g. @{@[David, me@icloud.com], German (de)}
+ @return NSDictionary
 */
 +(NSDictionary *)contributors;
+
+//Extended functions
+
+/**
+ Returns all projects
+ @param token The authToken
+ @note Returns the projects in this formate @[@{ProjectName : ProjectIP}]
+ @return NSArray
+ */
++(NSArray *)projects:(NSString *)token;
+
+/**
+ Returns the available languages for the project in this formate: @[@[Language Name, Percentage, Language Code]]
+ @param projectID This is the project ID of the Project, please take a look at [POEditor projects];
+ @param token The authToken
+ @return NSArray
+ */
++(NSArray *)languagesForProjectID:(NSString *)projectID andToken:(NSString *)token;
+
+/**
+ Returns an array of all strings for the project.
+ @prarm projectID The Project ID please take a look at [POEditor projects];
+ @param langCode The language code of the country
+ @param token The authToken
+ @note Take a look at [[NSLocale preferredLanguages] objectAtIndex:0]
+ @return NSArray
+ */
++(NSArray *)stringsFromProjectID:(NSString *)projectID languageCode:(NSString *)langCode andToken:(NSString *)token;
+
+/**
+ This will commit a new term to your project
+ @param term The term you want to commit
+ @param nativeTerm This can be a blank string
+ @param projectID The project Id you want to commit the term
+ @param langCode The language code you want to commit the term
+ @param token The authToken
+ @return BOOL
+ */
++(BOOL)commitTerm:(NSString *)term andNativeTerm:(NSString *)nativeTerm projectID:(NSString *)projectID language:(NSString *)langCode andToken:(NSString *)token;
+
+/**
+ Returns the filepath of all unlocalized strings (saved as a .plist)
+ @return NSString
+ */
++(NSString *)unlocalizedPath;
+
+/**
+ Returns the filepath of all strings for the matching language (saved as a .plist)
+ */
++(NSString *)languagePath;
+
 
 @end
